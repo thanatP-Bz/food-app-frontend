3ac05f4cb8200eed5a2349b8660eb4d020e51fec
Thanat <bztzn512@gmail.com>
2024-12-03 11:44:55 -0500

    food-app-frontend

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Button>Click me test</Button>
+    </div>
+  );
+}
+
+export default App;
