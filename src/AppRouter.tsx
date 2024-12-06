931916b3ded6bccf76527cb03a07faf2a0fe9dc6
Thanat <bztzn512@gmail.com>
2024-12-06 15:11:07 -0500

    addedAuth0

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,10 +1,18 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import Layouts from "./layouts/Layout";
+import Homepage from "./pages/Homepage";
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<Layouts>HOME PAGE</Layouts>} />
+      <Route
+        path="/"
+        element={
+          <Layouts>
+            <Homepage />
+          </Layouts>
+        }
+      />
       <Route path="/user-profile" element={<span>USER PROFILE PAGE</span>} />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-
 import AppRoutes from "./AppRouter.tsx";
-
 import { BrowserRouter as Router } from "react-router-dom";
+import Auth0ProviderWithNavigate from "./auth/Auth0ProviderWithNavigate.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Router>
-      <AppRoutes />
+      <Auth0ProviderWithNavigate>
+        <AppRoutes />
+      </Auth0ProviderWithNavigate>
     </Router>
   </StrictMode>
 );
