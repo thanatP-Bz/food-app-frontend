6aa0942e8063a4fddb813b35b54f748d709823d7
Thanat <bztzn512@gmail.com>
2024-12-09 13:54:17 -0500

    addedMenu

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -1,16 +1,23 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from "./ui/button";
+import UserNameMenu from "./UserNameMenu";
 
 const MainNav = () => {
-  const { loginWithRedirect } = useAuth0();
+  const { loginWithRedirect, isAuthenticated } = useAuth0();
   return (
-    <Button
-      variant="ghost"
-      className="font-bold hover:text-orange-500 hover:bg-white text-lg"
-      onClick={async () => await loginWithRedirect()}
-    >
-      Login
-    </Button>
+    <span className="flex space-x-2 items-center">
+      {isAuthenticated ? (
+        <UserNameMenu />
+      ) : (
+        <Button
+          variant="ghost"
+          className="font-bold hover:text-orange-500 hover:bg-white text-lg"
+          onClick={async () => await loginWithRedirect()}
+        >
+          Login
+        </Button>
+      )}
+    </span>
   );
 };
 
