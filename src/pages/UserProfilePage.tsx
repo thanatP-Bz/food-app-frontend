c6f18e21630c1265a601f47d143811fb502e4f0a
Thanat <bztzn512@gmail.com>
2024-12-22 16:39:58 -0500

    addedGetUser

--- src/pages/UserProfilePage.tsx
+++ src/pages/UserProfilePage.tsx
@@ -1,7 +1,25 @@
+import { useUpdateMyUser, useGetMyUser } from "@/api/MyUserApi";
 import UserProfileForm from "@/forms/user-profile-form/UserProfileForm";
 
 const UserProfilePage = () => {
-  return <UserProfileForm />;
+  const { currentUser, isLoading: isUpdateLoading } = useGetMyUser();
+  const { updateUser, isLoading: isGetLoading } = useUpdateMyUser();
+
+  if (isGetLoading) {
+    return <span>Loading...</span>;
+  }
+
+  if (!currentUser) {
+    return <span>Unable to load user profile</span>;
+  }
+
+  return (
+    <UserProfileForm
+      currentUser={currentUser}
+      onSave={updateUser}
+      isLoading={isUpdateLoading}
+    />
+  );
 };
 
 export default UserProfilePage;
